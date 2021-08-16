b30dd1f1f851e8b820fa322d63f3326122c93e5e
haominshi <haominshi@gmail.com>
2021-08-16 09:41:10 -0400

    1.2.2: more notes on listing 06

--- fpinkotlin-parent/fpinkotlin-makingprogramssafer/src/main/kotlin/com/fpinkotlin/makingprogramssafer/listing06/Payment.kt
+++ fpinkotlin-parent/fpinkotlin-makingprogramssafer/src/main/kotlin/com/fpinkotlin/makingprogramssafer/listing06/Payment.kt
@@ -3,17 +3,25 @@ package com.fpinkotlin.makingprogramssafer.listing06
 
 data class Payment(val creditCard: CreditCard, val amount: Int) {
     fun combine(payment: Payment): Payment =
-            if (creditCard == payment.creditCard)
-                Payment(creditCard, amount + payment.amount)
-            else
-                throw IllegalStateException("Cards don't match.")
+        if (creditCard == payment.creditCard)
+            Payment(creditCard, amount + payment.amount)
+        else
+            throw IllegalStateException("Cards don't match.")
 
     companion object {
         fun groupByCard(payments: List<Payment>): List<Payment> =
-                payments.groupBy { it.creditCard } // -> <Card, Payment>
-                        .values // <Payment> [Payment(cc1, 5), Payment(cc1, 1), Payment(cc2, 5)]
-                        .map { it.reduce(Payment::combine) }    // scan, and if payments has the same card, we combine them
-                                                                // to one big payment
-                                                                // [Payment(cc1, 6), Payment(cc2, 5)]
-    }
+            payments.groupBy { it.creditCard } // -> <Card, Payment>
+                .values // <Payment> [Payment(cc1, 5), Payment(cc1, 1), Payment(cc2, 5)]
+                .map {
+                     it.reduce(Payment::combine)
+                    // Below is alternative syntax
+                    /**
+                     * it.reduce { payment1, payment2 ->
+                     *  payment1.combine(payment2)
+                     * }
+                     */
+                }
+    }   // scan, and if payments has the same card, we combine them
+        // to one big payment
+        // [Payment(cc1, 6), Payment(cc2, 5)]
 }
