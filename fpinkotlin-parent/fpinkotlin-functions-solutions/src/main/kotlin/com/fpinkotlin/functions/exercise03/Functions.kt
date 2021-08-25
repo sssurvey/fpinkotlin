8a403200304e450a28342faf6c011f5e7d6a0fda
haominshi <haominshi@gmail.com>
2021-08-25 13:33:23 -0400

    finished 3.2

--- fpinkotlin-parent/fpinkotlin-functions-solutions/src/main/kotlin/com/fpinkotlin/functions/exercise03/Functions.kt
+++ fpinkotlin-parent/fpinkotlin-functions-solutions/src/main/kotlin/com/fpinkotlin/functions/exercise03/Functions.kt
@@ -5,7 +5,51 @@ fun square(n: Int) = n * n
 
 fun triple(n: Int) = n * 3
 
-fun <T, U, V> compose(f: (U) -> V, g: (T) -> U): (T) -> V = { f(g(it)) }
+fun <T, U, V> compose(f: (U) -> V, g: (T) -> U): (T) -> V {
+    return {
+        f(g(it))
+    }
+}
 
-val add: (Int) -> (Int) -> Int = { a -> { b -> a + b} }
+// Same as the §compose()§ but verbose and clearer version
+fun <T, U, V> composeEasy(f: (U) -> V, g: (T) -> U): (T) -> V {
+    val function1 = f
+    val function2 = g
+    val returnFunction: (T) -> V = { T ->
+        function1(
+            function2(T)
+        )
+    }
+    return returnFunction
+}
 
+val add: (Int) -> (Int) -> Int = { a -> { b -> a + b } }
+
+fun main() {
+
+    fun intToString(int: Int): String {
+        return "$int"
+    }
+
+    fun stringToInt(string: String): Int {
+        return string.toInt()
+    }
+
+    val composed = compose(
+        ::stringToInt,
+        ::intToString
+    )
+
+    val composed2 = composeEasy(
+        ::stringToInt,
+        ::intToString
+    )
+
+    composed(123).also {
+        println(it)
+    }
+
+    composed2(321).also {
+        println(it)
+    }
+}
